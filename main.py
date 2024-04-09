f3610cf0af2763db98e9f5a24c7609ce8e42a09a
Vanellopie <144303329+Vanellopie@users.noreply.github.com>
2024-04-09 14:57:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ supabase_key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS
 
 supabase: Client = create_client(supabase_url, supabase_key)
 
-class NineAnime(BaseModel):
+class AnimeList(BaseModel):
     id: Optional[int] = None
     Name: Optional[str] = None
     Score: Optional[int] = None
@@ -20,40 +20,40 @@ class NineAnime(BaseModel):
     Type: Optional[str] = None
     Episodes: Optional[float] = None
     Source: Optional[str] = None
-
+    
 
 app = FastAPI()
 
-@app.post("/anime_list/", response_model=NineAnime)
-def create_anime_list(anime_list: NineAnime):
+@app.post("/anime_lists/", response_model=NineAnime)
+def create_anime_list(anime_list: AnimeList):
     data = anime_list.dict(exclude_unset=True)
-    inserted_data = supabase.table("anime_list").insert(data).execute()
+    inserted_data = supabase.table("anime_lists").insert(data).execute()
     if inserted_data.data:
         return inserted_data.data[0]
     else:
         raise HTTPException(status_code=400, detail="Error inserting data")
 
-@app.get("/anime_list/", response_model=List[NineAnime])
-def read_anime_list():
-    data = supabase.table("anime_list").select("*").execute()
+@app.get("/anime_lists/", response_model=List[AnimeList])
+def read_anime_lists():
+    data = supabase.table("anime_lists").select("*").execute()
     if data.data:
         return data.data
     else:
         raise HTTPException(status_code=400, detail="Error reading data")
     
 
-@app.put("/anime_list/{anime_list_id}", response_model=NineAnime)
+@app.put("/anime_lists/{anime_list_id}", response_model=AnimeList)
 def update_anime_list(anime_list_id: int, anime_list: AnimeList):
     data = anime_list.dict(exclude_unset=True)
-    updated_data = supabase.table("anime_list").update(data).eq("id", anime_list_id).execute()
+    updated_data = supabase.table("anime_lists").update(data).eq("id", anime_list_id).execute()
     if updated_data.data:
         return updated_data.data[0]
     else:
         raise HTTPException(status_code=400, detail="Error updating data")
 
-@app.delete("/anime_list/{anime_list_id}", response_model=List[NineAnime])
+@app.delete("/anime_lists/{anime_list_id}", response_model=List[AnimeList])
 def delete_anime_list(anime_list_id: int):
-    deleted_data = supabase.table("anime_list").delete().eq("id", anime_list_id).execute()
+    deleted_data = supabase.table("anime_lists").delete().eq("id", anime_list_id).execute()
     if deleted_data.data:
         return deleted_data.data
     else:
