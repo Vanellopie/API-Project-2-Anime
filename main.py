c39ae3233b35ffe13d5647ee02bdbeecd10f8996
Vanellopie <144303329+Vanellopie@users.noreply.github.com>
2024-04-09 13:46:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,60 @@
+from supabase import create_client, Client
+from pydantic import BaseModel
 from typing import Optional
+from fastapi import FastAPI, HTTPException
+from typing import List
 
-from fastapi import FastAPI
+# https://docs.render.com/deploy-fastapi
+
+supabase_url: str = "https://pudktqwdyxdtkyxgxqzj.supabase.co"
+supabase_key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1ZGt0cXdkeXhkdGt5eGd4cXpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAzODE1MzMsImV4cCI6MjAyNTk1NzUzM30._MzX-DYB7YOtSKrJPxUxXDSQFmS6HVBj0AdJJ6ShfWE"
+
+supabase: Client = create_client(supabase_url, supabase_key)
+
+class AnimeList(BaseModel):
+    id: Optional[int] = None
+    Name: Optional[str] = None
+    Score: Optional[int] = None
+    Genres: Optional[int] = None
+    English_name: Optional[str] = None
+    Type: Optional[str] = None
+    Episodes: Optional[float] = None
+    Source: Optional[str] = None
+
 
 app = FastAPI()
 
+@app.post("/AnimeList/", response_model=AnimeList)
+def create_anime_list(anime_list: AnimeList):
+    data = anime_list.dict(exclude_unset=True)
+    inserted_data = supabase.table("anime_list").insert(data).execute()
+    if inserted_data.data:
+        return inserted_data.data[0]
+    else:
+        raise HTTPException(status_code=400, detail="Error inserting data")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/anime_list/", response_model=List[AnimeList])
+def read_anime_list():
+    data = supabase.table("anime_list").select("*").execute()
+    if data.data:
+        return data.data
+    else:
+        raise HTTPException(status_code=400, detail="Error reading data")
+    
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.put("/anime_list/{anime_list_id}", response_model=AnimeList)
+def update_anime_list(anime_list_id: int, anime_list: AnimeList):
+    data = anime_list.dict(exclude_unset=True)
+    updated_data = supabase.table("anime_list").update(data).eq("id", anime_list_id).execute()
+    if updated_data.data:
+        return updated_data.data[0]
+    else:
+        raise HTTPException(status_code=400, detail="Error updating data")
+
+@app.delete("/anime_list/{anime_list_id}", response_model=List[AnimeList])
+def delete_anime_list(anime_list_id: int):
+    deleted_data = supabase.table("anime_list").delete().eq("id", anime_list_id).execute()
+    if deleted_data.data:
+        return deleted_data.data
+    else:
+        raise HTTPException(status_code=400, detail="Error deleting data")
